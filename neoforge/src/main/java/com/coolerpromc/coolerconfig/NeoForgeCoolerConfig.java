90a7c6d43131252b74114a4e0f37a4429d193386
CoolerProYT <veronlam1818@gmail.com>
2026-05-11 22:17:42 +0800

    Revert "26.1.2.7"
    This reverts commit 5b6747197226a5da9797d8f6f1312eb401e4c14a.

--- neoforge/src/main/java/com/coolerpromc/coolerconfig/NeoForgeCoolerConfig.java
+++ neoforge/src/main/java/com/coolerpromc/coolerconfig/NeoForgeCoolerConfig.java
@@ -9,9 +9,6 @@ import net.neoforged.neoforge.event.server.ServerStartingEvent;
 @Mod(Constants.MOD_ID)
 public class NeoForgeCoolerConfig {
     public NeoForgeCoolerConfig(IEventBus modEventBus) {
-    }
-
-    public static void init() {
         CoolerConfig.init();
         NeoForge.EVENT_BUS.addListener(NeoForgeCoolerConfig::onServerStarting);
     }
