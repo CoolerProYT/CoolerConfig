62c0bd047e4ecdb9f9d8d1f2303f882f1ee69957
CoolerProYT <veronlam1818@gmail.com>
2026-06-10 00:23:26 +0800

    21.1.0

--- neoforge/src/main/java/com/coolerpromc/coolerconfig/NeoForgeCoolerConfigClient.java
+++ neoforge/src/main/java/com/coolerpromc/coolerconfig/NeoForgeCoolerConfigClient.java
@@ -6,7 +6,7 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 
-@EventBusSubscriber(modid = Constants.MOD_ID, value = Dist.CLIENT)
+@EventBusSubscriber(modid = Constants.MOD_ID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
 public class NeoForgeCoolerConfigClient {
     @SubscribeEvent
     public static void onClientSetup(FMLClientSetupEvent event) {
